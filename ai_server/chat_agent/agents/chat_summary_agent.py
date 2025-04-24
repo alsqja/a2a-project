88d03bf646a60e35a14098b7ef85f4395fc65f07
alsqja <alsqha2626@gmail.com>
2025-04-24 16:57:49 +0900

    feat: add dockerfile

--- ai_server/chat_agent/agents/chat_summary_agent.py
+++ ai_server/chat_agent/agents/chat_summary_agent.py
@@ -8,6 +8,10 @@ async def chat_summary(chats, source_company, lead_company) -> str:
                 당신은 "{source_company.company_name}" 회사 소속 영업 담당자 입니다.
                 "{lead_company.company_name}" 회사 소속 buyer 와의 협상 내용을 보고해야 합니다.
                 대화 내역을 요약하고 마크다운 형식의 보고서를 출력하세요.
+                
+                **보고서 규칙**
+                1. 반드시 한국어로 작성할 것
+                2. 대화 내용을 요약하고 구매자의 니즈를 분석할 것
                 """,
     )
 
